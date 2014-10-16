19239cddc9ac833927a8ee6712dddae5037c397b
amaiafdez <afernandezvi13dw@ikzubirimanteo.com>
2014-10-16 12:21:30 +0200

    script sql

--- index.js
+++ index.js
@@ -107,20 +107,6 @@ app.post('/registro', function (req, res) {
             // no errors
             // console.log({"msg":"insert OK", "sql":sql});
             // res.json({"msg":"insert OK", "sql":sql});delimiter $$
-
-CREATE TABLE `usuario` (
-  `idusuario` varchar(15) NOT NULL,
-  `contrasena` varchar(200) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `mail` varchar(45) NOT NULL,
-  `localidades` varchar(45) DEFAULT NULL,
-  `grupos` varchar(45) DEFAULT NULL,
-  `foto_perfil` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
-
-
             res.render('index');
             // res.json(JSON.stringify(rows));
     });
